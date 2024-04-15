1c9f4af833431655b9be4e10d82a84008b7a2377
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-04-15 15:09:29 +0200

    commit

--- form.php
+++ form.php
@@ -110,24 +110,24 @@ include __DIR__ . '/includes/navbar.php';?>
     <div id="box">
       <form style="width: 500px" action="" method="post">   
         <div class="mb-3">
-          <label for="titolo" class="form-label">Titolo del libro</label>
+          <label for="titolo" class="form-label">Titolo del libro:</label>
           <input type="text" class="form-control" name="titolo" id="titolo" />
         </div>
         <div class="mb-3">
-          <label for="autore" class="form-label">Autore del libro</label>
+          <label for="autore" class="form-label">Autore del libro:</label>
           <input type="text" class="form-control" name="autore" id="autore" />
         </div>
         <div class="mb-3">
-          <label for="anno_pubblicazione" class="form-label">Anno di uscita</label>
+          <label for="anno_pubblicazione" class="form-label">Anno di uscita:</label>
           <input type="number" class="form-control" name="anno_pubblicazione" id="anno_pubblicazione" />
         </div>
         <div class="mb-3">
-          <label for="genere" class="form-label">Genere del libro</label>
+          <label for="genere" class="form-label">Genere del libro:</label>
           <input type="text" class="form-control" name="genere" id="genere" />
         </div>
  
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Invia</button>
 
 
 
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,16 +1,15 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary container">
   <div class="container-fluid">
-    <a class="navbar-brand" href="http://localhost:8080/Progetto%20U4-W1-D5/index.php">Libri</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="http://localhost:8080/Progetto%20U4-W1-D5/index.php">Home</a>
+          <a class="nav-link active fw-bold" aria-current="page" href="http://localhost:8080/Progetto%20U4-W1-D5/index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="http://localhost:8080/Progetto%20U4-W1-D5/form.php">Inserisci un nuovo libro</a>
+          <a class="nav-link fw-bold" href="http://localhost:8080/Progetto%20U4-W1-D5/form.php">Inserisci un nuovo libro</a>
         </li>
       </ul>
     </div>
